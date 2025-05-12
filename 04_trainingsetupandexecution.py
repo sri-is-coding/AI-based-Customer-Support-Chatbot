af2454cce7e8cc4602db312814f5230109691e5b
sri-is-coding <146610065+sri-is-coding@users.noreply.github.com>
2025-05-12 17:17:23 +0800

    Update 04_trainingsetupandexecution.py
    had small error , all codes has " \ ", removed it

--- 04_trainingsetupandexecution.py
+++ 04_trainingsetupandexecution.py
@@ -1,29 +1,30 @@
 from transformers import TrainingArguments
 
-training\_args = TrainingArguments(
-output\_dir="./zephyr-finetuned-bankbot",
-per\_device\_train\_batch\_size=1,
-gradient\_accumulation\_steps=4,
-num\_train\_epochs=3,
-learning\_rate=2e-4,
-fp16=True,
-logging\_steps=10,
-save\_strategy="epoch",
-report\_to="none"
+training_args = TrainingArguments(
+    output_dir="./zephyr-finetuned-bankbot",
+    per_device_train_batch_size=1,
+    gradient_accumulation_steps=4,
+    num_train_epochs=3,
+    learning_rate=2e-4,
+    fp16=True,
+    logging_steps=10,
+    save_strategy="epoch",
+    report_to="none"
 )
+
 from transformers import Trainer, DataCollatorForLanguageModeling
 
-data\_collator = DataCollatorForLanguageModeling(
-tokenizer=tokenizer,
-mlm=False
+data_collator = DataCollatorForLanguageModeling(
+    tokenizer=tokenizer,
+    mlm=False
 )
 
 trainer = Trainer(
-model=model,
-train\_dataset=tokenized\_dataset,
-args=training\_args,
-data\_collator=data\_collator,
-tokenizer=tokenizer
+    model=model,
+    train_dataset=tokenized_dataset,
+    args=training_args,
+    data_collator=data_collator,
+    tokenizer=tokenizer
 )
 
 trainer.train()
